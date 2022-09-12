9ef7c7ce1779103d02d3f97264f2d3b037d6aa06
harrisonmccomb <mccombha@msu.edu>
2022-09-12 18:54:28 -0400

    A test of exported components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
+import { Welcome } from './test';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+      <Welcome/>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
