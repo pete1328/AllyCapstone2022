3450d3a23f96c852c1d5fbd96779d6b926ffe747
harrisonmccomb <mccombha@msu.edu>
2022-09-14 17:07:32 -0400

    React router implemented

--- kudos/src/App.js
+++ kudos/src/App.js
@@ -1,17 +1,52 @@
 import './App.css';
-import { LoginButton } from './components/button';
+import { HomeButton, LoginButton } from "./components/button";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
-    <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
-      <p className="text-3xl text-gray-700 font-bold mb-5">
-        Welcome!
-      </p>
-      <p className="text-gray-500 text-lg pb-4">
-        React, Material UI, and Tailwind CSS in action
-      </p>
-      <LoginButton/>
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="login" element={<Login />} />
+      </Routes>
     </div>
+  );
+}
+
+function Home() {
+  return (
+    <>
+      <main>
+        <div className="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
+          <p className="text-3xl text-gray-700 font-bold mb-5">
+            Welcome!
+          </p>
+          <p className="text-gray-500 text-lg pb-4">
+            React, React Router, Material UI, and Tailwind CSS in action
+          </p>
+          <nav>
+            <Link to="/login">
+              <LoginButton/>
+            </Link>
+          </nav>
+        </div>
+      </main>
+    </>
+  );
+}
+
+function Login() {
+  return (
+    <>
+      <main>
+        <h2 className="py-6">Welcome to the login page!</h2>
+        <nav>
+          <Link to="/">
+            <HomeButton/>
+          </Link>
+        </nav>
+      </main>
+    </>
   );
 }
 
--- kudos/src/components/button.js
+++ kudos/src/components/button.js
@@ -1,16 +1,14 @@
 import React, { useState } from "react";
 import Button from '@mui/material/Button'
 
-export function CountButton() {
-  const [count, setCount] = useState(0);
-
+export function HomeButton() {
   return (
-      <Button variant="contained" onClick={() => {setCount(count + 1)}}>Hello World! {count}</Button>
+      <Button variant="contained">Home</Button>
   )
 }
 
 export function LoginButton() {
   return (
-      <Button variant="contained">Log In!</Button>
+      <Button variant="contained">Log In</Button>
   );
 }
