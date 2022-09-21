20b85e047e0fcc1542542dcf9a02abde40df43af
cse498 <cse498@cse498-desk-95.cse.msu.edu>
2022-09-21 15:01:47 -0400

    ogres are like onions

--- kudos/src/pages/KudosPage.js
+++ kudos/src/pages/KudosPage.js
@@ -1,20 +1,15 @@
 import React from "react";
+import cat from '../assets/cat.png';
 import { Link } from "react-router-dom";
 import { TextField } from "@mui/material";
 import { TryNowButton, BackButton, HomeButton, NextButton } from "../components/Button";
 
-export class KudosPage extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            
-        };
-    }
-
-    render() {
-        return (
-            <main>
-                <div className="w-screen h-screen flex justify-center">
+export function KudosPage () {
+    return (
+        <main>
+            <div className="flex justify-center h-screen w-screen">
+                <img className="z-10 fixed place-self-center" src={cat} alt="envelope"/>
+                <div className="z-0 fixed flex place-self-center">
                     <div className="place-self-center">
                         <div className="bg-white w-[878px] h-[556px] drop-shadow-xl rounded-lg p-10">
                             <div className="w-full flex justify-between">
@@ -23,7 +18,7 @@ export class KudosPage extends React.Component {
                                     <div className="bg-[#E5E5E5] w-[257px] h-[128px] rounded-lg">
                                         <div className="p-4">
                                             <p className="font-poppins text-[18px]">At a loss for words? Try our gratitude wizard.</p>
-                                            <div className="flex w-full justify-center py-2">
+                                            <div className="flex w-full justify-center py-1">
                                                 <TryNowButton/>
                                             </div>
                                         </div>
@@ -62,7 +57,7 @@ export class KudosPage extends React.Component {
                         </div>
                     </div>
                 </div>
-            </main>
-        )
-    }
+            </div>
+        </main>
+    )
 }
