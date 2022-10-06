bb5a825aff3a83162d005f1482d2a7b07d2e290a
Phil Yu <yu39466@gmail.com>
2022-10-05 21:48:14 -0400

    Express

---
+++ kudos-services/index.js
@@ -0,0 +1,19 @@
+const config = require('../config.json');
+const Sequelize = require('Sequelize');
+
+const sequelize = new Sequelize(config.dbname, config.user, config.password, {
+    host: config.host,
+    port: 3306,
+    maxConcurrentQueries: 100,
+    dialect: 'mysql',
+    dialectOptions: {
+        ssl:'Amazon RDS'
+    },
+    pool: { maxConnections: 5, maxIdleTime: 30},
+    language: 'en'
+})
+
+
+
+
+
