c686422aebaf304d43ace09dd3269181cf9c280b
Harrison McComb <mccombha@msu.edu>
2022-10-13 20:52:33 -0400

    get requests

--- kudos-services/controllers/index.js
+++ kudos-services/controllers/index.js
@@ -1,5 +1,48 @@
 const User = require('../models/user');
 const Appreciation = require('../models/appreciation')
+const { Op } = require("sequelize");
+
+const allUsers = async (req, res) => {
+    try {
+        const users = await User.findAll({
+            attributes: ["user_id", "first_name", "last_name"],
+        });
+        return res.status(201).json({
+            users,
+        });
+    } catch (error) {
+        return res.status(500).json({ error: error.message })
+    }
+}
+
+const allManagers = async (req, res) => {
+    try {
+        const managers = await User.findAll({
+            attributes: ["user_id", "first_name", "last_name"],
+            where: {
+                [Op.or]: [{ position: "Admin" }, { position: "Manager" }],  
+            }
+        });
+        return res.status(201).json({
+            managers,
+        });
+    } catch (error) {
+        return res.status(500).json({ error: error.message })
+    }
+}
+
+const allKudos = async (req, res) => {
+    try {
+        const kudos = await User.findAll({
+            attributes: ["user_id", "sent", "received"],
+        });
+        return res.status(201).json({
+            kudos,
+        });
+    } catch (error) {
+        return res.status(500).json({ error : error.message })
+    }
+}
 
 const createUser = async (req, res) => {
     try {
@@ -42,5 +85,8 @@ const addAppreciation = async (req, res) => {
 module.exports = {
     createUser, 
     validateUser, 
-    addAppreciation
+    addAppreciation,
+    allManagers,
+    allUsers,
+    allKudos,
 }
--- kudos-services/routes/users.js
+++ kudos-services/routes/users.js
@@ -3,10 +3,13 @@ const router= express.Router();
 const db = require('../util/database');
 const User = require('../models/user');
 const Sequelize = require('sequelize');
-const { createUser, validateUser }  = require("../controllers/index");
+const { createUser, validateUser, allManagers, allUsers, allKudos }  = require("../controllers/index");
 
 router.get('/', (req, res) => ('This is root!'))
 router.post('/user/create', createUser)
 router.get('/user/validate', validateUser)
+router.get('/allManagers', allManagers)
+router.get('/allUsers', allUsers)
+router.get('/allKudos', allKudos)
 
 module.exports = router;
