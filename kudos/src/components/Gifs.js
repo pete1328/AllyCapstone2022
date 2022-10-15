68ed1ce9d1db83afce623ab91a73e80bb7623f2a
Harrison McComb <mccombha@msu.edu>
2022-10-14 23:46:45 -0400

    gif library component

---
+++ kudos/src/components/Gifs.js
@@ -0,0 +1,9 @@
+export const gifLibrary = [
+    {id: 0, url: "", tags: []},
+    {id: 1, url: "", tags: []},
+    {id: 2, url: "", tags: []},
+    {id: 3, url: "", tags: []},
+    {id: 4, url: "", tags: []},
+    {id: 5, url: "", tags: []},
+    {id: 6, url: "", tags: []},
+]
