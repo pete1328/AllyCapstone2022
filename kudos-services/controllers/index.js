21bca6aaa770ed5a4935a922958c50fe1b6aaeb0
Harrison McComb <mccombha@msu.edu>
2022-10-14 23:02:46 -0400

    Increment sent

--- kudos-services/controllers/index.js
+++ kudos-services/controllers/index.js
@@ -1,6 +1,7 @@
 const User = require('../models/user');
 const Appreciation = require('../models/appreciation')
 const { Op } = require("sequelize");
+const sequelize = require('../util/database');
 
 const allUsers = async (req, res) => {
     try {
@@ -82,10 +83,20 @@ const addAppreciation = async (req, res) => {
     }
 }
 
+const incrementSent = async (req, res) => {
+    try {
+        const [result, metadata] = await sequelize.query(`UPDATE Users SET sent = sent + ${req.body.sent.toString()} WHERE user_id = ${req.body.user_id.toString()}`);
+        return res.status(201).json();
+    } catch (error) {
+        return res.status(500).json({ error: error.message})
+    }
+}
+
 module.exports = {
     createUser, 
     validateUser, 
     addAppreciation,
+    incrementSent,
     allManagers,
     allUsers,
     allKudos,
--- kudos-services/routes/users.js
+++ kudos-services/routes/users.js
@@ -3,11 +3,13 @@ const router= express.Router();
 const db = require('../util/database');
 const User = require('../models/user');
 const Sequelize = require('sequelize');
-const { createUser, validateUser, allManagers, allUsers, allKudos }  = require("../controllers/index");
+const { createUser, validateUser, allManagers, allUsers, allKudos, incrementSent }  = require("../controllers/index");
 
 router.get('/', (req, res) => ('This is root!'))
 router.post('/user/create', createUser)
 router.get('/user/validate', validateUser)
+router.put('/user/incrementSent', incrementSent)
+// router.put('/user/incrementReceived', incrementReceived)
 router.get('/allManagers', allManagers)
 router.get('/allUsers', allUsers)
 router.get('/allKudos', allKudos)
