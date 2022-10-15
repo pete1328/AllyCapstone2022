18eba61bce5cfc1822f12c9d9c5110f458ab0b41
vongpha3 <vongpha3@msu.edu>
2022-10-14 23:05:28 -0400

    incrementReceived updates a user's received total

--- kudos-services/controllers/index.js
+++ kudos-services/controllers/index.js
@@ -92,11 +92,21 @@ const incrementSent = async (req, res) => {
     }
 }
 
+const incrementReceived = async (req, res) => {
+    try {
+        const [result, metadata] = await sequelize.query(`UPDATE Users SET sent = sent + ${req.body.received.toString()} WHERE user_id = ${req.body.user_id.toString()}`);
+        return res.status(201).json();
+    } catch (error) {
+        return res.status(500).json({ error: error.message})
+    }
+}
+
 module.exports = {
     createUser, 
     validateUser, 
     addAppreciation,
     incrementSent,
+    incrementReceived,
     allManagers,
     allUsers,
     allKudos,
