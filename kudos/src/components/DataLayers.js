a79cbf33a4d8b003a7c63e79f1f4423c22a1c596
Harrison McComb <mccombha@msu.edu>
2022-11-12 10:03:16 -0500

    cleaned up dataLayer page, var for GETs

--- kudos/src/components/DataLayers.js
+++ kudos/src/components/DataLayers.js
@@ -12,75 +12,47 @@ import {
 // EX Code to help develop page:
 //      https://github.com/d3/d3-force, https://codesandbox.io/s/kfkj8?file=/demo.js, https://tomroth.com.au/fdg-link/
 //      deck.gl Example: https://codesandbox.io/s/0q0hx?file=/public/index.html
-//import { nodesData, linksData } from "../components/TestData.js"; // dummy data for nodes and links
-
-/* To collect the node and link dictionaries for the graph */
-/* function getNetworkData() {
-  const users_url = prefix + "/api/allUsers";
-  let nodesData = [];
-  let linksData = []; */
+//import { starterDataNodes, starterDataLinks } from "../components/TestData.js"; // dummy data for nodes and links
   
 // Note: To get radius size, need to know how many times id appears as source and target <=add together //
+/* Acquire user interaction data via GET request */
+// TO-DO change get request to be what we plan (this is an already created one) 11/4 Abby
 
-  /* Acquire user interaction data via GET request */
-  // TO-DO change get request to be what we plan (this is an already created one) 11/4 Abby
-  /* const populateUsers = () => {
-    axios.get(users_url, { params: {
-      user_id: '1',
-    }})
-    .then(response => {
-      // Establish data variables
-      nodesData = [{ id: "Myriel", group: 1 },
-     { id: "Napoleon", group: 1 },
-     { id: "Mlle.Baptistine", group: 1 }];
-     linksData = [
-       { source: "Napoleon", target: "Myriel", value: 1 },
-       { source: "Mlle.Baptistine", target: "Myriel", value: 8 }]; */
-      /* let temp = [];
-      response.data.users.forEach(element => {
-        let pair = {
-          name : element["first_name"] + " " + element["last_name"],
-          id : element["user_id"]
-        }
-        temp.push(pair);
-      }); */
-      //updateUsers(temp);
-    /* })
-    .catch(error => {
-      console.log(error);
-    });
-  }
-  
-  populateUsers();
-
-  return {
-    nodesData,
-    linksData
-  };
-} */
+let nodesData = [{ id: "Poach", radius: 5}, { id: "Abby", radius: 8}];
+let linksData = [{ source: "Abby", target: "Poach"}];
 
 export function Test() {
 
   // ENTIRE app breaks when this is uncommented
-  //const [allMessages, setAllMessages] = useState([]);
+  // const [nodesData, setNodesData] = useState([starterDataNodes]);
+  // const [linksData, setLinksData] = useState([starterDataLinks]);
 
   const graph_nodes_url = prefix + "/api/appreciations/userConnections";
   const graph_links_url = prefix + "/api/appreciations/links";
   const graph_text_url = prefix + "/api/user/nodeText";
   const graph_id_url = prefix + "/api/user/nodeIds";
 
-  const nodesData = [
+  // const nodesData = [
+  //   { id: "Myriel", radius: 4},
+  //   { id: "Napoleon", radius: 10},
+  //   { id: "Mlle.Baptistine", radius: 15},
+  //   { id: "Zenigata", radius: 4},
+  // ];
+
+  nodesData = [
+    { id: "Poach", radius: 5},
+    { id: "Abby", radius: 8},
     { id: "Myriel", radius: 4},
     { id: "Napoleon", radius: 10},
     { id: "Mlle.Baptistine", radius: 15},
     { id: "Zenigata", radius: 4},
   ];
-  
-  const linksData = [
-    { source: "Napoleon", target: "Mlle.Baptistine"},
-    { source: "Mlle.Baptistine", target: "Napoleon"},
-    { source: "Zenigata", target: "Napoleon"},
-  ];
+
+  // const linksData = [
+  //   { source: "Napoleon", target: "Mlle.Baptistine"},
+  //   { source: "Mlle.Baptistine", target: "Napoleon"},
+  //   { source: "Zenigata", target: "Napoleon"},
+  // ];
 
   // Radius Calculation
   // Get all user ids and loops through to find all connections associated with user
@@ -90,7 +62,12 @@ export function Test() {
     // Reference: https://blog.logrocket.com/using-axios-all-make-concurrent-requests/
     axios.all(urls.map((url) => axios.get(url)))
     .then(axios.spread((...responses) => {
-
+      // setNodesData([
+      //   { id: "Myriel", radius: 4},
+      //   { id: "Napoleon", radius: 10},
+      //   { id: "Mlle.Baptistine", radius: 15},
+      //   { id: "Zenigata", radius: 4},
+      // ]);
     }))
     .catch(error => {
         console.log("ERROR" + error);
@@ -106,7 +83,11 @@ export function Test() {
         let table_len = 0;
         table_len = response.data.kudos.length;
         console.log("TEST INSIDE", table_len);
-        //setAllMessages(table_len);
+        // setLinksData([
+        //   { source: "Napoleon", target: "Mlle.Baptistine"},
+        //   { source: "Mlle.Baptistine", target: "Napoleon"},
+        //   { source: "Zenigata", target: "Napoleon"},
+        //   ]);
     })
     .catch(error => {
         console.log("ERROR" + error);
@@ -131,20 +112,7 @@ export function Test() {
   populateNodesList();
   populateLinksList();
   populateTextList();
-
-  return {
-    nodesData,
-    linksData,
-  };
-
 }
-
-const test2 = Test().nodesData;
-const test3 = Test().linksData;
-
-console.log("HERE IS TEST2!" + test2);
-console.log("HERE IS TEST3!" + test3);
-
 
 // Establish forces
 const simulation = forceSimulation()
@@ -159,14 +127,14 @@ const simulation = forceSimulation()
 
 //const nodesData = getNetworkData().nodesData; //Abby 11/8
 //const linksData = getNetworkData().linksData; //Abby 11/8
-const data = (test2, test3) //9/26 may be the formatting that leads to ERROR
+const data = (nodesData, linksData) //9/26 may be the formatting that leads to ERROR
 
-simulation.nodes(test2); // graph nodes
-simulation.force("link").links(test3); // graph links
+simulation.nodes(nodesData); // graph nodes
+simulation.force("link").links(linksData); // graph links
 
 export function DataLayers() {
-    const [fnode, setFnode] = useState(test2);
-    const [flinks, setFlinks] = useState(test3);
+    const [fnode, setFnode] = useState(nodesData);
+    const [flinks, setFlinks] = useState(linksData);
   
     // What actually updates the graph
     const ticked = () => {
