b0deb3b726fdabc830fd260964bc36b7fcd45d94
Phillip <yu39466@gmail.com>
2022-11-16 12:47:16 -0500

    changed database_prefix, added brackets

--- kudos/src/index.js
+++ kudos/src/index.js
@@ -4,11 +4,12 @@ import { BrowserRouter } from "react-router-dom";
 import './index.css';
 import App from './App';
 
-export var database_prefix = "";
+export var database_prefix = "/ml/api";
 export var ml_prefix = "";
-if (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1")
+if (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1") {
     database_prefix = "http://localhost:3001";
     ml_prefix = "http://localhost:5000";
+}
 
 const container = document.getElementById('root');
 const root = createRoot(container);
