ad68d65f32811620415b07e9041a5d2600d3f632
harrisonmccomb <mccombha@msu.edu>
2022-11-16 11:29:36 -0500

    Health check

--- kudos-ml/base.py
+++ kudos-ml/base.py
@@ -6,16 +6,27 @@ app = Flask(__name__)
 cors = CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
+
+@app.route('/')
+def health_check():
+    response_body = {
+        "health_check": True
+    }
+
+    return response_body
+
+
 @app.route('/api/validate')
 def validate_message():
     message = request.args.get('message')
     result = PositivityCheck(message)
 
     response_body = {
-        "result": result 
+        "result": result
     }
 
     return response_body
+
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
